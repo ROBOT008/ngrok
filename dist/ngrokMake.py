b8367641a372dbb7d5b4e19d51492b1d99da8b9c
ROBOT <1160468016@qq.com>
2019-08-15 17:52:36 +0800

    添加脚本

--- dist/ngrokMake.py
+++ dist/ngrokMake.py
@@ -3,11 +3,27 @@ import sys
 
 www = "robot86.top"
 
+
+def writeCfg(tport):
+    fname = "ngrok" + tport + ".cfg"
+    fcontent = 'server_addr: "robot86.top:' + tport + '"'"\n"
+    fcontent = fcontent + "trust_host_root_certs: false \n"
+    print(fname)
+    print(fcontent)
+    f = open(fname, 'a+')
+    f.write(fcontent)
+    f.close()
+
+
 if __name__ == '__main__':
     httpPort = sys.argv[1]
     httpsPort = sys.argv[2]
     TunnelPort = sys.argv[3]
-    cmd = "screen ./ngrokd -domain=" + www + " -httpAddr=:" + httpPort + " -httpsAddr=:" + httpsPort + " -tunnelAddr=:" + TunnelPort
-    print(cmd)
-    os.system(cmd)
-
+    rootpath = os.path.abspath(os.path.dirname(os.getcwd()))
+    ccmd = "screen cd " + rootpath + "/bin"
+    cmd = "./ngrokd -domain=" + www + " -httpAddr=:" + httpPort + " -httpsAddr=:" + httpsPort + " -tunnelAddr=:" + TunnelPort
+    cccmd = ccmd + " &&" + cmd
+    rmfile = "sudo rm -rf *.cfg"
+    os.system(rmfile)
+    writeCfg(TunnelPort)
+    os.system(cccmd)
